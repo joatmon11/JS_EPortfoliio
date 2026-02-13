df629f193616cdb7d8d615449f1c7c907f7fa7a3
joatmon11 <joat.mon.11@gmail.com>
2026-02-12 22:55:27 -0800

    10:39 into Inegrating EmailJS but overlay not working suddenly

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+function contact() {
+    event.preventDefault(event);
+    // emailjs
+    //     .sendForm(
+    //         event.target
+    //         "DTAOKfmzhLP-5xspo"
+    //     ).then(() => {
+    //         console.log('This worked')
+    //     })
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += ".modal__overlay--visible"
+    setTimeout(() => {
+        loading.classList.remove("modal__overlay--visible")
+        success.classList += ".modal__overlay--visible"
+        console.log('It worked 1')
+    }, 1000);
 
+}
 
 
