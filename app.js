41d850e69031490573670669574b3aefeb1b693f
joatmon11 <joat.mon.11@gmail.com>
2026-02-13 16:30:11 -0800

    Finished Dark Mode - Everything working

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+let isModalOpen = false;
+let contrastToggle = false
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
 function contact(event) {
     event.preventDefault(event);
     const loading = document.querySelector('.modal__overlay--loading')
@@ -18,5 +30,17 @@ function contact(event) {
             )
         })
 }
+
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false
+        return document.body.classList.remove("modal--open")
+        
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+}
+
 
 
